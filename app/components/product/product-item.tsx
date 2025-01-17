8e6b5febddcf82899ffad59e2815c9773cf8bdf1
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-01-17 19:26:31 -0300

    feat: update product components to use dynamic data

--- app/components/product/product-item.tsx
+++ app/components/product/product-item.tsx
@@ -1,39 +1,76 @@
+import Image from "next/image";
+
 import { cn } from "@/app/lib/utils";
 
 import {
   Card,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/app/components/ui/card";
+import { Badge } from "@/app/components/ui/badge";
 import AddCartButton from "@/app/components/product/add-cart-button";
 import ProductDetailsButton from "@/app/components/product/product-details-button";
 
-import { ImageIcon } from "lucide-react";
+import { BadgePercentIcon } from "lucide-react";
 
-const ProductItem = () => {
+import { formatCurrency } from "@/app/helpers/formatCurrency";
+
+import { ProductWithTotalPrice } from "@/app/helpers/computeProductTotalPrice";
+
+interface ProductItemProps {
+  product: ProductWithTotalPrice;
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <Card>
       <CardHeader
-        className={cn("flex aspect-square items-center justify-center")}
+        className={cn(
+          "relative flex aspect-square items-center justify-center bg-gradient-to-t from-blue-200 to-transparent p-0.5",
+        )}
       >
-        <ImageIcon size={24} />
+        <Image
+          src={product.imageUrls[0]}
+          alt={product.name}
+          height={500}
+          width={500}
+          className="h-auto w-full max-w-xs rounded-md"
+        />
+
+        {product.discountPercentage > 0 && (
+          <Badge className={cn("absolute left-2.5 top-2.5 gap-1")}>
+            <BadgePercentIcon size={14} />
+            {product.discountPercentage}%
+          </Badge>
+        )}
       </CardHeader>
 
       <CardContent className={cn("space-y-1.5 p-2.5")}>
-        <CardTitle className={cn("line-clamp-1 text-base text-foreground")}>
-          Título
+        <CardTitle
+          className={cn("line-clamp-1 text-base font-semibold text-foreground")}
+        >
+          {product.name}
         </CardTitle>
 
-        <div>
-          <span className="text-xs text-muted-foreground line-through">
-            R$ 500,00
-          </span>
-          <p className="text-lg font-semibold text-primary">R$ 500,00</p>
+        <div className="flex items-center gap-1.5">
+          <p className="text-lg font-semibold text-primary">
+            {formatCurrency(Number(product.totalPrice))}
+          </p>
+          {product.discountPercentage > 0 && (
+            <span className="text-xs text-muted-foreground line-through">
+              {formatCurrency(Number(product.basePrice))}
+            </span>
+          )}
         </div>
 
+        <CardDescription className={cn("line-clamp-2")}>
+          {product.description}
+        </CardDescription>
+
         <AddCartButton />
-        <ProductDetailsButton id="123" />
+        <ProductDetailsButton id={product.id} />
       </CardContent>
     </Card>
   );
--- app/components/product/products-list.tsx
+++ app/components/product/products-list.tsx
@@ -1,19 +1,29 @@
 import ProductsListTitle from "@/app/components/product/products-list-title";
 import ProductItem from "@/app/components/product/product-item";
 
+import { computeProductTotalPrice } from "@/app/helpers/computeProductTotalPrice";
+
+import { Product } from "@prisma/client";
+
 interface ProductsListProps {
   title: string;
+  products: Product[];
+  limit?: number;
 }
 
-const ProductsList = ({ title }: ProductsListProps) => {
+const ProductsList = ({ title, products, limit }: ProductsListProps) => {
+  const displayedProducts = limit ? products.slice(0, limit) : products;
+
   return (
     <section className="space-y-2.5">
       <ProductsListTitle title={title} />
 
       <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-        <li>
-          <ProductItem />
-        </li>
+        {displayedProducts.map((product) => (
+          <li key={product.id}>
+            <ProductItem product={computeProductTotalPrice(product)} />
+          </li>
+        ))}
       </ul>
     </section>
   );
