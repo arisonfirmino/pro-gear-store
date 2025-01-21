f719d98973ae604c84a1bb365dedee7d6342c11c
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-01-21 15:07:41 -0300

    feat: add logo and update favicon

--- app/components/header/title.tsx
+++ app/components/header/title.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Title = () => {
   return (
     <Link href="/">
-      <h1 className="hidden text-nowrap text-2xl font-bold lg:flex">
-        ProGear Store
-      </h1>
+      <div className="flex items-center gap-2">
+        <Image
+          src="/logo.png"
+          alt="ProGear Store"
+          height={500}
+          width={500}
+          className="min-h-10 min-w-10 max-w-10 rounded-full"
+        />
+
+        <h1 className="hidden text-nowrap text-2xl font-bold xl:flex">
+          ProGear Store
+        </h1>
+      </div>
     </Link>
   );
 };
